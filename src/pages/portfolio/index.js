9e97a82db2b68208265e6b579c435bf4db832379
stevenmarc <stevenmarcfr@gmail.com>
2019-03-21 15:37:43 +0100

    Add react-grid-layout

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,8 +1,29 @@
 import React from "react"
+import GridLayout from "react-grid-layout"
 
 class portfolioIndex extends React.Component {
   render() {
-    return <h2>Portfolio</h2>
+    var layout = [
+      { i: "a", x: 0, y: 0, w: 1, h: 2 },
+      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: "c", x: 4, y: 0, w: 1, h: 2 },
+    ]
+    return (
+      <React.Fragment>
+        <h2>Portfolio</h2>
+        <GridLayout
+          className="layout"
+          layout={layout}
+          cols={12}
+          rowHeight={30}
+          width={1200}
+        >
+          <div key="a">a</div>
+          <div key="b">b</div>
+          <div key="c">c</div>
+        </GridLayout>
+      </React.Fragment>
+    )
   }
 }
 
