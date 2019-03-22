51f20b155de59a77226b3725401811a21e04aa70
stevenmarc <stevenmarcfr@gmail.com>
2019-03-22 16:38:16 +0100

    Add google fonts via helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import anime from "animejs"
+import { Helmet } from "react-helmet"
 
 const backgroundImage = require("../assets/images/steven_bg.jpg")
 const bgImageStyle = {
@@ -44,32 +45,43 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="cComponent" style={bgImageStyle}>
-        <h1
-          className="ml3"
-          style={{
-            fontFamily: "Relevant,Helvetica,Arial,sans-serif",
-            fontWeight: 100,
-            fontSize: "2em",
-            padding: "40px 20px 0px 40px",
-            color: "white",
-          }}
-          dangerouslySetInnerHTML={{ __html: wrappedLetters }}
-        />
-        <h2
-          style={{
-            fontFamily: "Relevant,Helvetica,Arial,sans-serif",
-            fontWeight: "lighter",
-            fontSize: "1.5em",
-            padding: "0px 20px 20px 40px",
-            color: "white",
-          }}
-        >
-          Freelance developer, writer and filmmaker. <br />I love finding
-          creative solutions with clients <br />
-          to help them connect with their audience, <br />
-          wether it be by code, text, or moving images.
-        </h2>
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>stevenmarc.me</title>
+          <link rel="canonical" href="http://stevenmarc.me" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Raleway|Roboto:100"
+            rel="stylesheet"
+          />
+        </Helmet>
+        <div className="cComponent" style={bgImageStyle}>
+          <h1
+            className="ml3"
+            style={{
+              fontFamily: "Raleway, sans-serif",
+              fontWeight: 100,
+              fontSize: "2em",
+              padding: "180px 20px 0px 40px",
+              color: "white",
+            }}
+            dangerouslySetInnerHTML={{ __html: wrappedLetters }}
+          />
+          <h2
+            style={{
+              fontFamily: "Roboto, sans-serif",
+              fontWeight: 100,
+              fontSize: "1.5em",
+              padding: "0px 20px 20px 40px",
+              color: "white",
+            }}
+          >
+            Freelance developer, writer and filmmaker. <br />I love finding
+            creative solutions with clients <br />
+            to help them connect with their audience, <br />
+            whether it be by code, text, or moving images.
+          </h2>
+        </div>
       </div>
     )
   }
