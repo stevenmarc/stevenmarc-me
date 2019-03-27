9d20ef309220352abe2b8b8d2701c75ded46a799
stevenmarc <stevenmarcfr@gmail.com>
2019-03-27 12:18:11 +0100

    Add gatsby-image

--- src/gatsby-config.js
+++ src/gatsby-config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+},
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      path: `${__dirname}/src/assets/images`,
+      name: "images",
+    },
+  }
