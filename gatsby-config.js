c950ad7f0a10222360d142d85564fe8930eeba68
stevenmarc <stevenmarcfr@gmail.com>
2019-03-28 12:59:01 +0100

    Add offline and source-filesystem plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,35 @@
 module.exports = {
   siteMetadata: {
     title: 'stevenmarc.me',
-    description: 'Steven-Marc Couchouron developer portfolio'
+    description: 'Steven-Marc Couchouron developer portfolio',
   },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-  ],
-},{
-  resolve: `gatsby-plugin-manifest`,
-  options: {
-    name: `stevenmarc.me`,
-    short_name: `stevenmarc.me`,
-    start_url: `/`,
-    background_color: `#f7f0eb`,
-    theme_color: `#ea7623`,
-    // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-    // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-    display: `standalone`,
-    icon: `src/images/icon.png`, // This path is relative to the root of the site.
-    include_favicon: true, // Include favicon
-  },
-},
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      path: `${__dirname}/src/assets/images`,
-      name: 'images',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `stevenmarc.me`,
+        short_name: `stevenmarc.me`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#ea7623`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
+      },
     },
-  }
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+        name: `images`,
+      },
+    },
+  ],
+}
