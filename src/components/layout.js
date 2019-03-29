67c6f7d956985ed3468db43a99475f8879f1136f
stevenmarc <stevenmarcfr@gmail.com>
2019-03-29 15:00:39 +0100

    Replace gobal.css with GlobalStyle

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import Header from './header'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import colors from '../../colors'
+
+const GlobalStyle = createGlobalStyle`
+*,
+*::after,
+*::before { 
+  -webkit-box-sizing: inherit;
+  box-sizing: inherit;
+  }
+html {
+  background-color: rgb(29, 29, 29);
+}
+body {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box; 
+  overflow-x: hidden;
+  margin: 0;
+  color: ${colors.primary};
+  font-family: Quicksand, sans-serif;
+  font-weight: 300;
+}
+a {
+  color: #ffffff;
+  text-decoration: none;
+  padding-left: 1em;
+}
+
+a:hover {
+  color: #ea7623;
+}
+
+.logotext:hover {
+  fill: #ea7623;
+}
+`
 
 export default ({ children }) => (
-  <div
-    style={{
-      margin: `0`,
-      padding: `0`,
-    }}
-  >
+  <div>
+    <GlobalStyle />
     <Header />
     {children}
   </div>
