6841c416e8512b4a64b8812157a8bc8ab1342cf4
stevenmarc <stevenmarcfr@gmail.com>
2019-03-29 10:52:27 +0100

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <form name="contact" netlify>
+      <p>
+        <label>
+          Name <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </Layout>
+)
