077b10a5de6a7eb4cf478bd9b6c20479a4beff81
stevenmarc <stevenmarcfr@gmail.com>
2019-04-01 16:30:27 +0200

    Add context in page queries

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -4,10 +4,16 @@ import Layout from './layout'
 
 export default class postLayout extends Component {
   render() {
+    const { markdownRemark } = this.props.data
     return (
       <Layout>
         <div>
-          <h1>Post Layout</h1>
+          <h1>{markdownRemark.frontmatter.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: markdownRemark.html,
+            }}
+          />
         </div>
       </Layout>
     )
@@ -15,8 +21,8 @@ export default class postLayout extends Component {
 }
 
 export const query = graphql`
-  query PostQuery {
-    markdownRemark(frontmatter: { slug: { eq: "/third-post" } }) {
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
       frontmatter {
         title
